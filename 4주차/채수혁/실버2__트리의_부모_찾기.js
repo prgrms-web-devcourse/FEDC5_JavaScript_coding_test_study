a4bd6e3c44836952268a9acf9a9143ffa4fb78bc
ienrum <azang0506@gmail.com>
2023-12-19 15:59:05 +0900

    Refactor: 실버2 트리의 부모 찾기

--- 4주차/채수혁/실버2__트리의_부모_찾기.js
+++ 4주차/채수혁/실버2__트리의_부모_찾기.js
@@ -14,8 +14,10 @@ const input = process.execArgv.includes("--stack-size=65536")
 
 const inputs = input.trim().split("\n");
 let cnt = 0;
+// n: 노드의 개수
 const n = Number(inputs[0]);
 const arr = inputs.slice(1).map((v) => v.split(" ").map((v2) => Number(v2)));
+// 양방향 그래프
 const map = {};
 arr.forEach((v) => {
   if (map[v[0]]) map[v[0]].push(v[1]);
@@ -23,20 +25,28 @@ arr.forEach((v) => {
   if (map[v[1]]) map[v[1]].push(v[0]);
   else map[v[1]] = [v[0]];
 });
+// 부모 노드를 저장할 배열
 const parents = Array.from({ length: n + 1 }, () => 0);
+// 방문 여부를 저장할 배열
 const visited = Array.from({ length: n + 1 }, () => false);
 
+// 1번 노드부터 탐색 시작
 const que = [[1, map[1]]];
 
 while (que.length > 0) {
+  // cur: nodeArr 의 부모노드 , nodeArr: 부모 노드와 연결된 노드들의 배열
   const [cur, nodeArr] = que.shift();
-
   visited[cur] = true;
 
   for (const node of nodeArr) {
+    // 이미 방문한 노드라면 continue
     if (visited[node]) continue;
+    // 특정 노드번호의 부모노드를 parents 라는 부모 배열에 저장
     parents[node] = cur;
+    // 현재 노드가 부모노드가 되어서 다음 노드를 탐색
     que.push([node, map[node]]);
   }
 }
+
+// 출력
 console.log(parents.slice(2).reduce((a, v) => a + v + "\n", ""));
