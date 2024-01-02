88ff4a684bad2e64bc51a4a8cd78ac8218c1891d
kimyou1102 <yoosk5485@naver.com>
2024-01-02 12:12:21 +0900

    Feat: 힙 실버2 최소 힙

---
+++ 5주차/김유경/실버2__최소_힙_1927.js
@@ -0,0 +1,138 @@
+const fs = require("fs");
+const filePath =
+  process.platform === "linux" ? "/dev/stdin" : `${__dirname}/input.txt`;
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const n = Number(input.shift());
+const arr = input.map((x) => Number(x.trim()));
+const answer = [];
+
+class MinHeap {
+  constructor() {
+    this.items = [];
+  }
+
+  swap(index1, index2) {
+    let temp = this.items[index1]; // items의 index1의 값을 temp(임시공간)에 담음
+    this.items[index1] = this.items[index2]; // index1에 index2의 값을 저장
+    this.items[index2] = temp; // index2에 아까 index1의 값을 temp에 넣어놓은 값을 저장
+  }
+
+  //부모 인덱스 구하는 메소드
+  parentIndex(index) {
+    return Math.floor((index - 1) / 2);
+  }
+
+  //왼쪽 자식 인덱스 구하는 메소드
+  leftChildIndex(index) {
+    return index * 2 + 1;
+  }
+
+  //오른쪽 자식 인덱스 구하는 메소드
+  rightChildIndex(index) {
+    return index * 2 + 2;
+  }
+
+  //부모 노드 구하는 메소드
+  parent(index) {
+    return this.items[this.parentIndex(index)];
+  }
+
+  //왼쪽 자식 노드 구하는 메소드
+  leftChild(index) {
+    return this.items[this.leftChildIndex(index)];
+  }
+
+  //오른쪽 자식 노드 구하는 메소드
+  rightChild(index) {
+    return this.items[this.rightChildIndex(index)];
+  }
+
+  //최대 힙의 경우 최댓값을 반환하고 최소 힙의 경우 최솟값을 반환하는 메소드
+  peek() {
+    return this.items[0];
+  }
+
+  //힙의 크기(항목 개수)를 반환하는 메소드
+  size() {
+    return this.items.length;
+  }
+
+  // MinHeap 클래스는 Heap 클래스를 상속받았으므로 Heap 클래스의 메소드를 모두 사용할 수 있다.
+  bubbleUp() {
+    let index = this.items.length - 1;
+
+    while (
+      this.parent(index) !== undefined &&
+      this.parent(index) > this.items[index]
+    ) {
+      this.swap(index, this.parentIndex(index));
+      index = this.parentIndex(index);
+    }
+  }
+
+  bubbleDown() {
+    let index = 0;
+
+    while (
+      this.leftChild(index) !== undefined &&
+      (this.leftChild(index) < this.items[index] ||
+        this.rightChild(index) < this.items[index])
+    ) {
+      let smallerIndex = this.leftChildIndex(index);
+
+      if (
+        this.rightChild(index) !== undefined &&
+        this.rightChild(index) < this.items[smallerIndex]
+      ) {
+        smallerIndex = this.rightChildIndex(index);
+      }
+
+      this.swap(index, smallerIndex);
+
+      index = smallerIndex;
+    }
+  }
+
+  add(item) {
+    this.items[this.items.length] = item;
+    this.bubbleUp();
+  }
+
+  poll() {
+    let item = this.items[0]; // 첫번째 원소 keep
+    this.items[0] = this.items[this.items.length - 1]; // 맨 마지막 원소를 첫번째 원소로 복사
+    this.items.pop(); // 맨 마지막 원소 삭제
+    this.bubbleDown();
+
+    return item; // keep해둔 값 반환
+  }
+}
+
+const minHeap = new MinHeap();
+minHeap.add(1);
+minHeap.add(10);
+minHeap.add(5);
+minHeap.add(100);
+minHeap.add(8);
+console.log(minHeap); //array(5) [1, 8, 5, 100, 10]
+console.log(minHeap.poll()); // 1
+console.log(minHeap.poll()); // 5
+console.log(minHeap.poll()); // 8
+console.log(minHeap.poll()); // 10
+console.log(minHeap.poll()); // 100
+console.log(minHeap.poll()); // 100
+console.log(minHeap); // array(0)
+// for (let i = 0; i < n; i++) {
+//   if (arr[i] !== 0) {
+//     minHeap.add(arr[i]);
+//   } else {
+//     if (minHeap.size() === 0) {
+//       answer.push(0);
+//     } else {
+//       answer.push(minHeap.poll());
+//     }
+//   }
+// }
+
+// console.log(answer.join("\n"));
